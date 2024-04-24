b5a7da946f90f49a7a7602d6072a1d4e5809419a
Kaustuv Karki <karkikaustuv@gmail.com>
2024-04-25 04:07:31 +0545

    Completed login without jwt

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -16,6 +16,20 @@ export const register = async (req, res, next) => {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-    next(errorHandler(500, error.message));
+    return next(errorHandler(500, error.message));
+  }
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email: email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = await validUser.password;
+    if (validPassword !== password)
+      return next(errorHandler(400, "Invalid password"));
+    res.status(200).json({ message: "User logged in successfully" });
+  } catch (error) {
+    return next(errorHandler(500, error.message));
   }
 };
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { register } from "../controllers/user.controller.js";
+import { register, login } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;
