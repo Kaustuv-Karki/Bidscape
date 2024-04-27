44a83b3a5a5b592eb201ce3d6ea6c41f7ad7c2f6
Kaustuv Karki <karkikaustuv@gmail.com>
2024-04-28 02:46:22 +0545

    redux setup and login functionlity

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loginStart, loginSuccess } from "../redux/user/userSlice";
+const Login = () => {
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(loginStart());
+      const response = await fetch(`/api/user/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      const { userDetails, token } = data;
+      localStorage.setItem("token", token);
+      dispatch(loginSuccess(userDetails));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(formData);
+
+  return (
+    <div className="flex mx-auto w-[400px] h-[400px] bg-white flex-col items-center justify-center my-[10%] rounded-sm">
+      <h1 className="py-2 text-[2rem] font-semibold text-slate-700">LOGIN</h1>
+      <form
+        className="mt-8 px-4 py-4 flex flex-col gap-6"
+        onSubmit={handleSubmit}>
+        <div className="flex flex-col">
+          <label className="w-8 text-[1.15rem] font-semibold text-slate-600">
+            Email
+          </label>
+          <input
+            className="w-[300px] py-2 px-4 rounded-md bg-[#EFEEEC] outline-none"
+            type="email"
+            name="email"
+            onChange={handleChange}
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="w-8 text-[1.15rem] font-semibold text-slate-600 ">
+            Password
+          </label>
+          <input
+            className="w-[300px] py-2 px-4 rounded-md bg-[#EFEEEC] outline-none"
+            type="password"
+            name="password"
+            onChange={handleChange}
+            placeholder="Enter Password"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-[300px] py-2 px-4 bg-[#D6482C] text-white font-semibold rounded-md">
+          LOGIN
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ frontend/src/redux/user/userSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  loading: false,
+  error: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginStart: (state) => {
+      state.loading = true;
+    },
+    loginSuccess: (state, action) => {
+      state.user = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.loading = false;
+      state.error = null;
+    },
+  },
+});
+
+export const { loginStart, loginSuccess, logout } = userSlice.actions;
+
+export default userSlice.reducer;
