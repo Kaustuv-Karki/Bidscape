65ab75f35ed42901b14803b4dbef93118d7a1b85
Kaustuv Karki <karkikaustuv@gmail.com>
2024-04-28 18:39:41 +0545

    Some homepage chnages

---
+++ frontend/src/components/ProjectCard.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+
+/* 
+budget
+: 
+50000
+createdAt
+: 
+"2024-04-26T18:06:24.233Z"
+description
+: 
+"This is the description"
+duration
+: 
+1
+email
+: 
+"this@gmail.com"
+images
+: 
+(2) ['image1', 'image2']
+skills
+: 
+(2) ['Frontend', 'Backend']
+tags
+: 
+(2) ['Good', 'Bad']
+title
+: 
+"This is the title of the project haha"
+updatedAt
+: 
+"2024-04-26T18:06:24.233Z"
+userId
+: 
+"662bed0112a6fefc4ef13d83"
+__v
+: 
+0
+_id
+: 
+"662bed2012a6fefc4ef13d86"
+*/
+
+const ProjectCard = ({ projectDetails }) => {
+  const { title, description, budget, duration, skills, tags, images, email } =
+    projectDetails;
+  return (
+    <div className="h-[450px] bg-white w-[320px] rounded-md cursor-pointer hover:shadow-lg transition-shadow py-4 px-4">
+      <p className="font-bold py-4 text-[1.2rem] text-center px-2 text-[#D6482C]">
+        {title}
+      </p>
+      <div className="flex px-4 py-2 gap-4 flex-wrap">
+        {tags.map((tag, index) => {
+          return (
+            <p
+              key={index}
+              className="px-4 py-1 border rounded-md border-[#D6482C] text-[#D6482C] font-semibold">
+              {tag}
+            </p>
+          );
+        })}
+      </div>
+      <p className="text-left px-4 mt-4 text-slate-600 line-clamp-2 font-semibold">
+        {description}
+      </p>
+      <div>
+        <p className="text-left px-4 mt-4 text-slate-600 font-semibold">
+          Skills Required:
+        </p>
+        <div className="flex px-4 py-2 gap-4 flex-wrap">
+          {skills.map((skill, index) => {
+            return (
+              <p
+                key={index}
+                className="px-4 py-1 border rounded-md border-[#D6482C] text-[#D6482C] font-semibold">
+                {skill}
+              </p>
+            );
+          })}
+        </div>
+      </div>
+      <p className="text-left px-4 mt-4 text-slate-600 font-semibold">
+        Duration: {duration} days
+      </p>
+      <p className="text-left px-4 mt-4 text-slate-600 font-semibold">
+        Budget: ${budget}
+      </p>
+      <p className="text-left px-4 mt-4 text-slate-600 font-semibold">
+        Contact: {email}
+      </p>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Bidding from "../assets/Bidding.svg";
+import ProjectCard from "../components/ProjectCard";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [projects, setProjects] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const response = await fetch("/api/project/get", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      setProjects(data);
+      console.log(data);
+    };
+    fetchProjects();
+  }, []);
+  return (
+    <div className="max-w-[1200px] mx-auto">
+      <div className="flex items-center justify-between mt-16">
+        <div className="flex flex-col text-left max-w-[400px]">
+          <h1 className="text-[2.5rem] font-bold">
+            Biscape <br />{" "}
+            <span className="text-[#D6482C]">
+              Where Projects Find Their Perfect Match
+            </span>
+          </h1>
+          <p className="text-slate-700 font-semibold py-4">
+            Discover seamless project management and talent acquisition on
+            BidScape. Join our vibrant community today!
+          </p>
+          <div className="flex gap-4">
+            <button
+              className="py-2 px-6 bg-[#D6482C] text-white font-semibold rounded-md"
+              onClick={() => navigate("/register")}>
+              Sign Up
+            </button>
+            <button
+              className="py-2 px-6 bg-white text-[#D6482C] font-semibold rounded-md"
+              onClick={() => navigate("/login")}>
+              Log In
+            </button>
+          </div>
+        </div>
+        <img className="max-w-[600px]" src={Bidding} alt="Bidding" />
+      </div>
+      <div className="mt-12">
+        <h1 className="text-[#D6482C] font-bold text-[2.5rem]">
+          Current Projects
+        </h1>
+        <div>
+          {projects.map((project) => {
+            return <ProjectCard key={project._id} projectDetails={project} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
