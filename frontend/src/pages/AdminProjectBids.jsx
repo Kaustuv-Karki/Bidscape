8eea159d258707ab0fdb7c3c9a2187b2c021d7ab
Kaustuv Karki <karkikaustuv@gmail.com>
2024-05-01 19:17:28 +0545

    Completed

--- frontend/src/pages/AdminProjectBids.jsx
+++ frontend/src/pages/AdminProjectBids.jsx
@@ -5,27 +5,44 @@ const AdminProjectBids = () => {
   const params = useParams();
   const { id } = params;
   const [bids, setBids] = useState([]);
+  const getBidsById = async () => {
+    try {
+      const response = await fetch(`/api/project/bid/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      const data = await response.json();
+      setBids(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    const getBidsById = async () => {
-      try {
-        const response = await fetch(`/api/project/bid/${id}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        });
-        const data = await response.json();
-        setBids(data);
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
     getBidsById();
   }, []);
+
+  const handleChange = async (bidId, value) => {
+    try {
+      const response = await fetch(`/api/admin/update/${bidId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify({ status: value }),
+      });
+      const data = await response.json();
+      console.log(data);
+      getBidsById();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
@@ -44,15 +61,31 @@ const AdminProjectBids = () => {
                 <p>{bid.coverLetter}</p>
                 <p>{bid.amount}</p>
               </div>
-              <div
-                className={`${
-                  bid.status === "pending"
-                    ? "bg-orange-500"
-                    : bid.status === "accepted"
-                    ? "bg-green-500"
-                    : "bg-red-500"
-                } py-2 px-4 text-white rounded-md`}>
-                {bid.status}
+              <div className="flex flex-col items-center gap-4">
+                <div
+                  className={`${
+                    bid.status === "pending"
+                      ? "bg-orange-500"
+                      : bid.status === "accepted"
+                      ? "bg-green-500"
+                      : "bg-red-500"
+                  } py-2 px-4 text-white rounded-md`}>
+                  {bid.status.slice(0, 1).toUpperCase() + bid.status.slice(1)}
+                </div>
+                {bid.status === "pending" && (
+                  <div className="flex gap-4">
+                    <div
+                      className="bg-green-500 px-3 py-2 text-white rounded-md cursor-pointer"
+                      onClick={() => handleChange(bid._id, "accepted")}>
+                      Accept
+                    </div>
+                    <div
+                      className="bg-red-500 px-3 py-2 text-white rounded-md cursor-pointer"
+                      onClick={() => handleChange(bid._id, "rejected")}>
+                      Reject
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           );
